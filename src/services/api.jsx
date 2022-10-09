6bbf92567b2b94d3b10241dafb48bdd1fcdc5cf2
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-10-09 21:55:01 +0300

    api

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,73 @@
+ import axios from "axios";
  
- 
- /*
- key a69e735adbbc0b61afd22bfa0224f7db
+const KEY = 'a69e735adbbc0b61afd22bfa0224f7db';
+const BASE_URL = 'https://api.themoviedb.org/3';
 
-      https://api.themoviedb.org/3/movie/550?api_key=a69e735adbbc0b61afd22bfa0224f7db
-   */ 
+const TREND_URL = `${BASE_URL}/trending/movie/week`;
+const SEARCH_URL = `${BASE_URL}/search/movie`;
+const MOVIE_ID_URL = `${BASE_URL}/movie/`;
+
+const fetchTrendData = async () => {
+   try {
+      const { data } = await axios.get(
+         `${TREND_URL}?api_key=${KEY}&page=1`);
+      return data.results;
+   } catch (error) {
+   console.error('Something wrong! Can not get full trends' + error);
+   }
+};
+
+const fetchSearchData = async value => {
+   try {
+      const { data } = await axios.get(
+         `${SEARCH_URL}?api_key=${KEY}&query=${value}&page=1`
+      );
+      return data.results;
+   } catch (error) {
+      console.error('Something wrong! Can not search films' + error);
+   }
+};
+
+const fetchDataById = async id => {
+   try {
+      const { data } = await axios.get(
+         `${MOVIE_ID_URL}${id}?api_key=${KEY}`
+      );
+      return data;
+   } catch (error) {
+      console.error('Something wrong! Can not search films by ID' + error);
+   }
+};
+
+const fetchDataByCast = async id => {
+   try {
+      const { data } = await axios.get(
+         `${MOVIE_ID_URL}${id}/credits?api_key=${KEY}`
+      );
+      return data;
+   } catch (error) {
+      console.error('Something wrong! Can not search films by ID' + error);
+   }
+};
+
+const fetchDataByReviews = async id => {
+   try {
+      const { data } = await axios.get(
+         `${MOVIE_ID_URL}${id}/reviews?api_key=${KEY}`
+      );
+      return data;
+   } catch (error) {
+      console.error('Something wrong! Can not search films by ID' + error);
+   }
+};
+
+export {
+   fetchTrendData,
+   fetchSearchData,
+   fetchDataById,
+   fetchDataByCast,
+   fetchDataByReviews,
+}
+
+
+  
