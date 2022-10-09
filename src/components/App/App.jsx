12bc05992d763b163f5abfddb581b002a519a236
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-10-09 23:38:30 +0300

    h

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
-//import {Layout} from '../Layout/Layout'
+import {Layout} from 
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path='/' element={<div>Layout</div>}>
-
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='movies' element={<MovieDataPage /> } />
         </Route>
       </Routes>
 
