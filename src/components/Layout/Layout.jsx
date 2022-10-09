68defb6d7c680d5df61cb8c8622d457e7d2dcab1
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-10-09 20:26:10 +0300

    h

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,11 +3,10 @@
 const Layout = () => {
     return (
         <>
-            <div>Home</div>
-            <div>Muvies</div>
+          
         </>
-    )
-}
+    );
+};
 
 
 export default Layout;
