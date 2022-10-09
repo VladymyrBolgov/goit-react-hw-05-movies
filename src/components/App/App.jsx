00887dcadb5335a5327b31eb7d98634239d02193
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-10-10 00:26:13 +0300

    layout app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,28 +1,30 @@
-import { Route, Routes } from 'react-router-dom'
-import {Layout} from 
+import { Route, Routes } from 'react-router-dom';
+import Layout from '../Layout/Layout';
+import PageNotFound from '../../pages/PageNotFound';
+import Home from '../../pages/Home/Home';
+import Cast from '../../pages/Cast/Cast';
+import Reviews from '../../pages/Reviews/Reviews'
+import { lazy } from 'react';
+  
+const Movies = lazy(() => import('../../pages/Movies/Movies'));
+const MovieDetails = lazy(() => import('../../pages/MovieDetails/MovieDetails'))
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path='/' element={<Layout />}>
+          <Route path="*" element={<PageNotFound />} />
           <Route index element={<Home />} />
-          <Route path='movies' element={<MovieDataPage /> } />
+          <Route path='movies' element={<Movies />} />
+          <Route path='movies/:movieId' element={<MovieDetails />}>
+            <Route path='cast' element={<Cast />} />
+            <Route path='reviews' element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
-
-      <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    ></div>
     </>
-  )
-}
+  );
+};
 
 export default App;
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,18 @@
+import NavBar from '../NavBar/NavBar';
+import { Outlet } from 'react-router-dom';
+import { Div } from '../Wrappers/Wrappers';
+import { Suspense } from 'react';
 
 
 const Layout = () => {
     return (
-        <>
-          
-        </>
+        <Div>
+            <NavBar />
+            <Suspense fallback={null}>
+                <Outlet />
+           </Suspense>
+        </Div>
     );
 };
-
 
 export default Layout;
