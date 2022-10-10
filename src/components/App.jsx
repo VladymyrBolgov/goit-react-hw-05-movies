eb95918bf3b09df82124f30d66fda418783340ba
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-10-10 18:36:43 +0300

    t

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
             <Route path='cast' element={<Cast />} />
             <Route path='reviews' element={<Reviews />} />
           </Route>
-          
+
         </Route>
       </Routes>
     </>
