00806b21d2673b6745562bc6cfb9a5041a64ca0b
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-10-11 12:13:14 +0300

    g

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,19 +3,19 @@ import { NavItem } from './styled';
 
 const NavBar = () => {
     return (
-        <Div display="flex" p={3} boxShadow="md">
-            <P mr="4">
-                <NavItem to={'/'} end key={'home'}>
-                    Home
-                </NavItem>
-            </P>
-            <P>
-                <NavItem to={'movies'} key={'movies'}>
-                    Movies
-                </NavItem>
-            </P>
-        </Div>
-    )
-}
+      <Div display="flex" p={3} boxShadow="md">
+        <P mr="4">
+          <NavItem to={'/'} end key={'home'}>
+            Home
+          </NavItem>
+        </P>
+        <P>
+          <NavItem to={'movies'} key={'movies'}>
+            Movies
+          </NavItem>
+        </P>
+      </Div>
+    );
+  };
 
 export default NavBar;
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -3,26 +3,27 @@ import { Formik, Field, Form, ErrorMessage } from 'formik';
 import PropTypes from 'prop-types';
 
 const SearchBar = ({ onSubmitFilter, query }) => {
-    
+  
     return (
-        <Div m="4" fontSize='m'>
-            <Formik
-                initialValues={{ search: query }}
-                onSubmit={(values, { resetForm }) => {
-                    onSubmitFilter(values.search);
-                }}>
-                <Form>
-                    <Field style={{ fontSize: "2rem" }} name="search" type="text" required autoComplete='off' />
-                    <ErrorMessage name="firstName" />
-                    <P fontSize='l' m='1' as='button' type="submit">Search</P>
-                </Form>
-            </Formik>
-        </Div>
+      <Div m="4" fontSize='m'>
+        <Formik
+          initialValues={{ search: query }}
+          onSubmit={(values, { resetForm }) => {
+            onSubmitFilter(values.search);
+          }}
+        >
+          <Form>
+            <Field   style={{ fontSize: "2rem" }}  name="search" type="text" required autoComplete='off' />
+            <ErrorMessage name="firstName" />
+            <P fontSize='l' m='1' as='button' type="submit">Search</P>
+          </Form>
+        </Formik>
+      </Div>
     );
-};
-
-SearchBar.propTypes = {
-    onSubmitFilter: PropTypes.func.isRequired,
-}
-
-export default SearchBar;
+  };
+  
+  SearchBar.propTypes = {
+      onSubmitFilter: PropTypes.func.isRequired,
+  }
+  
+  export default SearchBar;
