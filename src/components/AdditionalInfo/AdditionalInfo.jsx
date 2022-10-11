d595ed36d4fcfe7e9970544a6a2fa5addf7378c7
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-10-11 12:04:28 +0300

    n

--- src/components/AdditionalInfo/AdditionalInfo.jsx
+++ src/components/AdditionalInfo/AdditionalInfo.jsx
@@ -1,22 +1,22 @@
 import { Div, P } from 'components/Wrappers/Wrappers';
 import { StyledInfoLink } from 'components/commonStyled';
 
-const AdditionalInfo = ({ linkRef }) => {
+const AdditionalInfo = ({linkRef}) => {
     return (
-        <Div borderBottom="1px solid" borderTop="1px solid">
-            <P mx="4" fontSize="m">
-                Additional information
-            </P>
-            <ul>
-                <li>
-                    <StyledInfoLink to="cast" state={{ from: linkRef }}>Cast</StyledInfoLink>
-                </li>
-                <li>
-                    <StyledInfoLink to="reviews" state={{ from: linkRef }}>Reviews</StyledInfoLink>
-                </li>
-            </ul>
-        </Div>
+      <Div borderBottom="1px solid" borderTop="1px solid">
+        <P mx="4" fontSize="m">
+          Additional information
+        </P>
+        <ul>
+          <li>
+            <StyledInfoLink to="cast" state= {{ from: linkRef}}>Cast</StyledInfoLink>
+          </li>
+          <li>
+            <StyledInfoLink to="reviews" state= {{ from: linkRef}}>Reviews</StyledInfoLink>
+          </li>
+        </ul>
+      </Div>
     );
-};
+  };
 
 export default AdditionalInfo;
