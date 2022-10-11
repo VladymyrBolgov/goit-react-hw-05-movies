f94aeae4cc8c53f9f9103fb58527e79cb0a1fe98
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-10-11 22:33:49 +0300

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,31 @@
-import { Route, Routes } from 'react-router-dom';
-import Layout from './Layout/Layout';
-import PageNotFound from '../pages/PageNotFound';
-import Home from '../pages/Home/Home';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home/Home.jsx';
 import Cast from '../pages/Cast/Cast';
-import Reviews from '../pages/Reviews/Reviews'
+import Reviews from '../pages/Reviews/Reviews';
+import Layout from '../components/Layout/Layout';
+import PageNotFound from '../pages/PageNotFound';
 import { lazy } from 'react';
-  
-const Movies = lazy(() => import('../pages/Movies/Movies'));
-const MovieDetails = lazy(() => import('../pages/MovieDetails/MovieDetails'))
 
-const App = () => {
+const Movies = lazy(() => import('../pages/Movies/Movies'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails/MovieDetails'));
+
+export const App = () => {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route path="*" element={<PageNotFound />} />
           <Route index element={<Home />} />
-          <Route path='movies' element={<Movies />} />
+          <Route path="movies" element={<Movies />} />
 
-          <Route path='movies/:movieId' element={<MovieDetails />}>
-            <Route path='cast' element={<Cast />} />
-            <Route path='reviews' element={<Reviews />} />
+          <Route path="movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
-
+          
         </Route>
+       
       </Routes>
     </>
   );
 };
-
-export default App;
