ce858423911513cc637beed5d6b70ae859ca9095
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-10-11 22:30:39 +0300

    sear

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,29 +1,29 @@
-import { Div, P } from 'components/Wrappers/Wrappers'
-import { Formik, Field, Form, ErrorMessage } from 'formik'; 
-import PropTypes from 'prop-types';
+import { Div,P } from 'components/Wrappers/Wrappers';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import PropTypes from 'prop-types'
 
 const SearchBar = ({ onSubmitFilter, query }) => {
   
-    return (
-      <Div m="4" fontSize='m'>
-        <Formik
-          initialValues={{ search: query }}
-          onSubmit={(values, { resetForm }) => {
-            onSubmitFilter(values.search);
-          }}
-        >
-          <Form>
-            <Field   style={{ fontSize: "2rem" }}  name="search" type="text" required autoComplete='off' />
-            <ErrorMessage name="firstName" />
-            <P fontSize='l' m='1' as='button' type="submit">Search</P>
-          </Form>
-        </Formik>
-      </Div>
-    );
-  };
-  
-  SearchBar.propTypes = {
-      onSubmitFilter: PropTypes.func.isRequired,
-  }
-  
-  export default SearchBar;
+  return (
+    <Div m="4" fontSize='m'>
+      <Formik
+        initialValues={{ search: query }}
+        onSubmit={(values, { resetForm }) => {
+          onSubmitFilter(values.search);
+        }}
+      >
+        <Form>
+          <Field   style={{ fontSize: "2rem" }}  name="search" type="text" required autoComplete='off' />
+          <ErrorMessage name="firstName" />
+          <P fontSize='l' m='1' as='button' type="submit">Search</P>
+        </Form>
+      </Formik>
+    </Div>
+  );
+};
+
+SearchBar.propTypes = {
+    onSubmitFilter: PropTypes.func.isRequired,
+}
+
+export default SearchBar;
