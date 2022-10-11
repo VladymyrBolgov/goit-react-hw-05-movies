39248f9a7ad7b804846e7ee8c363a05ff03be06f
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-10-11 11:25:05 +0300

    cast

---
+++ src/pages/Cast/Cast.jsx
@@ -0,0 +1,37 @@
+import {fetchDataByCast} from 'services/api'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {Div, P} from 'components/Wrappers/Wrappers'
+
+const Cast = () => {
+
+    const { movieId } = useParams
+    const [cast, setCast] = useState([]);
+
+    useEffect(() => {
+        fetchDataByCast(movieId).then(setCast)
+    }, [movieId])
+    
+    return (
+        <Div>
+            <Div as="ul" display="flex" width="1/1" p="0" flexWrap="wrap">
+                {cast.map(actor => (
+                    <Div as="li" m="2" key={actor.id} width={138}>
+                        <img
+                            src={
+                                actor.profile_path
+                                    ? `https://image.tmdb.org/t/p/w138_and_h175_face${actor.profile_path}`
+                                    : 'https://via.placeholder.com/138x175.png'
+                            }
+                            alt={actor.name}
+                        />
+                        <P fontWeight="bold">{actor.name}</P>
+                        <P>{actor.character}</P>
+                    </Div>
+                ))}
+            </Div>
+        </Div>
+    );
+};
+
+export default Cast;
