c5de0690f85bd3eb7cdcdf8d7278035149c72395
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2023-01-24 02:19:58 +0200

    -

--- src/components/Wrappers/Wrappers.jsx
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-import { space, layout, color, flexbox, typography, border, shadow } from "styled-system"
-
-export const Div = styled('div')(color, space, layout, flexbox, typography, border, shadow);
-
-export const P = styled('p')(color, space, layout, flexbox, typography);
